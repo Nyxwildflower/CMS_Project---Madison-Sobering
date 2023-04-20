28fa534e04647c0f1009926102eaaaf0e99ee122
MSobering <madisonsobering@gmail.com>
2023-04-19 21:32:27 -0500

    Started adding styles to pages, minor edits

--- create.php
+++ create.php
@@ -1,14 +1,11 @@
 <?php
     require('connect.php');
     require('authorize.php');
-    
-    $query = "SELECT * FROM categories";
-    $statement = $db->prepare($query);
-    $statement->execute();
 
     if($_POST){
-        $current_timestamp = date("F d, Y, h:i a");
-        $title = filter_input(INPUT_POST, "text", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        date_default_timezone_set("America/Winnipeg");
+        $current_timestamp = date("Y-m-d h-i-s");
+        $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $title = trim($title);
         $content = filter_input(INPUT_POST, "content", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $content = trim($content);
@@ -24,31 +21,47 @@
         $create_page->bindValue('category_id', $category_id, PDO::PARAM_INT);
         $create_page->execute();
 
+
+        // Need to check character limits on title and content
         header("Location: index.php");
         exit("Page created.");
     }
+
+    $category_query = "SELECT * FROM categories";
+    $categories = $db->prepare($category_query);
+    $categories->execute();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Create Page</title>
 </head>
-<body>    
-    <form action="create.php" method="post">
+<body>
+    <?php include('header.php') ?>    
+
+    <form class="container" action="create.php" method="post">
         <label for="title">Title</label>
-        <input name="title" type="text"/>
+        <input class="form-control" name="title" type="text"/>
 
         <label for="content"></label>
-        <textarea id="create_content" name="content" id="" rows="10"></textarea>
+        <textarea class="form-control" id="create_content" name="content" id="" rows="10"></textarea>
 
-        <label for="select_category">Select Category that this page best fits:</label>
-        <select name="select_category" id="select_category">
-            <?php while($category = $statement->fetch()): ?>
-                <option value="<?= $category['category_id'] ?>"><?= $category['category_name'] ?></option>
+        <label for="select_category">Select a category that this page best fits</label>
+        <select class="form-control" name="select_category" id="select_category">
+            <?php while($category = $categories->fetch()): ?>
+                    <option value="<?= $category['category_id'] ?>"><?= $category['category_name'] ?></option>
             <?php endwhile ?>
         </select>
-        <button type="submit">Submit</button>
+
+        <button class="btn btn-success" type="submit">Submit</button>
     </form>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
     require('connect.php');
 
-    $page_query = "SELECT p.*, c.*, u.username FROM pages p JOIN users u ON (u.user_id = p.user_id) JOIN categories c ON (c.category_id = p.category_id) LIMIT 1";
+    $page_query = "SELECT p.*, c.*, u.username FROM pages p JOIN users u ON (u.user_id = p.user_id) JOIN categories c ON (c.category_id = p.category_id) WHERE p.page_id = :id LIMIT 1";
+
+    $id = isset($_GET['page_id']) ? $_GET['page_id'] : 2;
 
     $statement = $db->prepare($page_query);
+    $statement->bindValue('id', $id, PDO::PARAM_INT);
     $statement->execute();
     $page = $statement->fetch();
 
@@ -20,15 +23,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>PDO SELECT</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Museum of Experience</title>
 </head>
-<body>    
-    <h2><?= $page['title'] ?></h2>
-    <h3>By: <?= $page['username'] ?></h3>
-    <h3><?= $page['category_name'] ?></h3>
-    <div><?= $page['content'] ?></div>
-    <a href="create.php">Create Page</a>
-    <div>
+<body>
+    <?php include('header.php') ?>
+
+    <div class="container">
+        <h2><?= $page['title'] ?></h2>
+        <h3>By: <?= $page['username'] ?></h3>
+        <h3><?= $page['category_name'] ?></h3>
+        <div><?= $page['content'] ?></div>
+        <div><?= $page['created'] ?></div>
+    </div>
+
+    <div class="container">
+        <a class="btn btn-dark" href="create.php">Create Page</a>
+        <a class="btn btn-dark" href="edit.php?page_id=<?= $page['page_id'] ?>">Edit Page</a>
+        <a class="btn btn-dark" href="delete.php?page_id=<?= $page['page_id'] ?>">Delete Page</a>
+    </div>
+
+    <div class="container">
+        <h2>Comments</h2>
         <?php while($comment = $comments->fetch()): ?>
             <div>
                 <h3><?= $comment['username'] ?></h3>
@@ -36,5 +55,8 @@
             </div>
         <?php endwhile ?>
     </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html> 
