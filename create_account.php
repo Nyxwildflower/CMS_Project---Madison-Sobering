ea6f0a879a212b63257bf92bf4b5b8549d0876a3
MSobering <madisonsobering@gmail.com>
2023-04-28 17:08:57 -0500

    Death of the mysterious one (1)

--- create_account.php
+++ create_account.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     require('connect.php');
 
     $input = "";
@@ -59,8 +59,6 @@
             $add_user->execute();
             header("Location: index.php");
             exit("User created.");
-        }else{
-            // header("Location: create_account.php");
         }
     }
 ?>
@@ -75,9 +73,9 @@
     <title>Create Account</title>
 </head>
 <body>
-    <?= include('header.php') ?>   
+    <?php include('header.php') ?>
 
-    <div class="container"> 
+    <main class="container"> 
             <h2>Create Account</h2>
             <form action="create_account.php" method="post">
                 <label for="email">Email</label>
@@ -99,10 +97,10 @@
         <?php for($i = 0; $i < count($errors); $i++): ?>
             <p class="alert alert-danger"><?= $errors[$i] ?></p>
         <?php endfor ?>
-    </div>
+    </main>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
-</html> 
+</html>
