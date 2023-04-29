9450913717bcaabfd8988610b8cb4f56bab86f02
MSobering <madisonsobering@gmail.com>
2023-04-28 19:47:11 -0500

    Delete image when deleting page

--- delete.php
+++ delete.php
@@ -1,9 +1,22 @@
 <?php
+    // Deletes a page based off of the page_id passed. If an image exists with it, it's also deleted.
     require('connect.php');
     require('check_session.php');
+    require('image_upload.php');
 
     if(isset($_GET['page_id']) && filter_input(INPUT_GET, 'page_id', FILTER_VALIDATE_INT)){
-        $page_id = $_GET['page_id'];
+        $page_id = filter_input(INPUT_GET, 'page_id', FILTER_SANITIZE_NUMBER_INT);
+
+        // Removes any image associated with the row from the images file.
+        $image_exists = "SELECT image_file FROM pages WHERE page_id = :page_id LIMIT 1";
+        $image_goes_too = $db->prepare($image_exists);
+        $image_goes_too->bindValue('page_id', $page_id, PDO::PARAM_INT);
+        $image_goes_too->execute();
+
+        if($image_goes_too->rowCount() > 0){
+            $image_to_delete = $image_goes_too->fetch();
+            delete_file($image_to_delete['image_file']);
+        }
 
         $query = "DELETE FROM pages WHERE page_id = :page_id LIMIT 1";
         $statement = $db->prepare($query);
